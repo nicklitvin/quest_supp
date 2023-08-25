0892701664d18498cbc5b617cd4c2d007136b1f6
nicklitvin <nick.litvinov9@gmail.com>
2023-08-25 16:59:28 -0700

    updated shellscript

--- backup.sh
+++ backup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # MySQL credentials and settings
+DB_HOST="your_host"
+DB_PORT=3306
 DB_USER="your_username"
 DB_PASSWORD="your_password"
 DB_NAME="your_database_name"
 
 # Output directory
-OUTPUT_DIR="/saved_data"
+OUTPUT_DIR="~/saved_data"
 
 # Create output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
@@ -16,6 +18,6 @@ TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 BACKUP_FILE="$OUTPUT_DIR/${DB_NAME}_${TIMESTAMP}.sql"
 
 # Perform mysqldump
-mysqldump -u"$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$BACKUP_FILE"
+mysqldump -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" --password="$DB_PASSWORD" "$DB_NAME" > "$BACKUP_FILE"
 
 echo "Backup completed: $BACKUP_FILE"
